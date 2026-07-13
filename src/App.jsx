02c1ef4ff885dcffc9f656370a5cf074ff952402
TDShah26 <f20210994@goa.bits-pilani.ac.in>
2026-07-13 20:10:09 +0530

    feat: integrate vercel analytics in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from './components/Hero'
 import Bio from './components/Bio'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App() {
   return (
     <>
       <Hero />
       <Bio />
+      <Analytics />
     </>
   )
 }
